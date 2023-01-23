73d2c59e168655a9512988fddc3f3ee9c06edd9f
Mixxmaster <dundukovmi@gmail.com>
2023-01-23 18:42:47 +0600

    add connect params, add test endpoint 'get users'

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/user-model');
 
 const PORT = process.env.PORT || 3000;
 const URL = `mongodb+srv://mixxmaster:1324354657q@testdb.mhzyzno.mongodb.net/?retryWrites=true&w=majority`;
@@ -10,7 +11,7 @@ app.use(express.json());
 
 mongoose
   .set('strictQuery', false) //fix Mongoose deprecation warning
-  .connect(URL)
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.log(`DB connection error: ${error}`));
 
@@ -21,3 +22,11 @@ app.listen(PORT, error => {
 const handleError = (res, error) => {
   res.status(500).json({ error });
 };
+
+app.get('/users', (req, res) => {
+  User.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => handleError(res, 'Что-то пошло не так'));
+});
